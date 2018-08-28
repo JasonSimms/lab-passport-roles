67374fbe6ce7f48d868043db6ee10c38b4b740dc
gitgut0110 <jsimms0110@gmail.com>
2018-08-28 14:38:33 +0200

    Boss Delete by Email working

--- routes/boss.js
+++ routes/boss.js
@@ -1,27 +1,80 @@
-const express = require('express');
-const boss  = express.Router();
-const User =  require('../models/User')
+const express = require("express");
+const boss = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 
-/* GET home page */
-boss.get('/boss', (req, res, next) => {
-    res.send('bosspage')
-//   res.render('index');
+/* GET BOSS CREATE PAGE */
+boss.get("/boss", (req, res, next) => {
+  if (req.user.role !== "Boss") {
+    res.send("no way!");
+  } else next();
 });
 
-boss.get('/viewemployee', (req, res, next) => {
-    const { id } = req.params
-    console.log(req.params, '------------------')
-    User.findById(id).then(result => {
-        if (!result) return res.send('No such bear')
-        if (result.user.toString() !== req.user._id.toString() && req.user.role !== 'Boss') {
-            res.send('NOT YOUR BEAR!')
-        }
-        res.send("yay ")
+boss.get("/boss", (req, res, next) => {
+  // res.send('bosspage')
+  res.render("bossemployeecreate");
+});
+
+boss.post("/boss/create", (req, res, next) => {
+  console.log("attempt to create -----------------");
+  console.log(req.body);
+  const { email, password, role } = req.body;
+
+  const encrypted = bcrypt.hashSync(password, 10);
+
+  new User({ email, password: encrypted, role })
+    .save()
+    .then(result => {
+      res.send("User account was created");
     })
-
-
-    res.send('employee create page')
-//   res.render('index');
+    .catch(err => {
+      if (err.code === 11000) {
+        return res.render("sign-up", { error: "user exists already" });
+      }
+      console.error(err);
+      res.send("something went wrong");
+    });
 });
+
+// DELETEPAGE
+
+boss.get("/boss/delete", (req, res, next) => {
+    if (req.user.role !== "Boss") {
+      res.send("no way!");
+    } else next();
+  });
+
+boss.get("/boss/delete", (req, res, next) => {
+  // res.send('bosspage')
+  res.render("bossdeleteemployee");
+});
+
+boss.post("/boss/delete", (req, res, next) => {
+  console.log("attempt to delete -----------------");
+//   const { email } = req.body;
+  console.log(req.body.email);
+//   const encrypted = bcrypt.hashSync(password, 10);
+  User.findOneAndRemove(
+    {email: req.body.email}).then(data => {
+        console.log(data, '-----beeeppppp---')
+    //   res.redirect("/");
+    })
+  });
+
+// boss.get('/viewemployee', (req, res, next) => {
+//     const { id } = req.params
+//     console.log(req.params, '------------------')
+//     User.findById(id).then(result => {
+//         if (!result) return res.send('No such bear')
+//         if (result.user.toString() !== req.user._id.toString() && req.user.role !== 'Boss') {
+//             res.send('NOT YOUR BEAR!')
+//         }
+//         res.send("yay ")
+//     })
+
+//     res.send('employee create page')
+// //   res.render('index');
+// });
 
 module.exports = boss;
